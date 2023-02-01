e3a7c86e70dc4d2b00d70ed98267e06cc0bfeb47
redvital <kervisvasquez24@gmail.com>
2023-02-01 19:17:37 -0400

    primer com

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,3 @@ Route::middleware([
 // Route::resource('categories', [CategoryController::class])->names('categories');
 Route::resource('categories', CategoryController::class )->except('show')->names('categories');
 Route::resource('stores', StoreController::class )->except('show')->names('stores');
-// commented
